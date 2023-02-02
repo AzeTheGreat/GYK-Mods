b589e277fa47d3b18d6ed2ae8112b68018fc259e
Aze <36392269+AzeTheGreat@users.noreply.github.com>
2023-02-01 20:36:08 -0700

    Feat: Enable interpolation on the player
    GYK moves the player using Rigidbody2D.MovePosition.  This is done in
    FixedUpdate, so if the frame and update rates don't match, movement
    will be inconsistent.  This is only obvious when the camera is tracking
    an object.  Interpolation causes the transform to be properly updated
    each frame.

---
+++ src/PlayerJudderFix/FixJudder.cs
@@ -0,0 +1,14 @@
+﻿using HarmonyLib;
+using UnityEngine;
+
+namespace PlayerJudderFix
+{
+    [HarmonyPatch(typeof(PlayerComponent), nameof(PlayerComponent.InitLocalPlayer))]
+    class EnableInterpolation
+    {
+        static void Postfix(PlayerComponent __instance)
+        {
+            __instance.wgo.GetComponent<Rigidbody2D>().interpolation = RigidbodyInterpolation2D.Interpolate;
+        }
+    }
+}
