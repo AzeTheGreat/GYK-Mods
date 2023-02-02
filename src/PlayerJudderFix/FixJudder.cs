9b349f3e22e50c03daf9f39d80b8efdc9cb419a8
Aze <36392269+AzeTheGreat@users.noreply.github.com>
2023-02-01 20:43:03 -0700

    Fix: Sprite Z positioning breaks interpolation
    Change sprite Z positioning to apply to a child GO instead of the
    parent GO that is driven by the RigidBody2D.
    
    When a driven transform is externally updated, it breaks interpolation.

--- src/PlayerJudderFix/FixJudder.cs
+++ src/PlayerJudderFix/FixJudder.cs
@@ -1,5 +1,9 @@
-﻿using HarmonyLib;
+﻿using AzeLib.Extensions;
+using HarmonyLib;
+using System.Collections.Generic;
+using System.Reflection.Emit;
 using UnityEngine;
+using UnityEngine.Rendering;
 
 namespace PlayerJudderFix
 {
@@ -9,6 +13,34 @@ namespace PlayerJudderFix
         static void Postfix(PlayerComponent __instance)
         {
             __instance.wgo.GetComponent<Rigidbody2D>().interpolation = RigidbodyInterpolation2D.Interpolate;
+        }
+    }
+
+    [HarmonyPatch(typeof(RoundAndSortComponent), nameof(RoundAndSortComponent.DoUpdateStuff))]
+    class FixPlayerMovement
+    {
+        static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> codes)
+        {
+            var position_setter = AccessTools.PropertySetter(typeof(Transform), nameof(Transform.position));
+
+            return codes.Manipulator(
+                i => i.Calls(position_setter),
+                i => new[]
+                {
+                    new CodeInstruction(OpCodes.Ldarg_0),
+                    CodeInstruction.Call(typeof(FixPlayerMovement), nameof(Wrapper))
+                });
+        }
+
+        private static void Wrapper(Transform transform, Vector3 position, RoundAndSortComponent roundSort)
+        {
+            if (roundSort._world_part?.parent?.is_player ?? false)
+            {
+                var tf = transform.GetComponentInChildren<SortingGroup>().transform;
+                tf.position = new(tf.position.x, tf.position.y, position.z);
+            }
+            else
+                transform.position = position;
         }
     }
 }
