ded805b39ff865ef1efc666e79a5b4b530ac71a9
Aze <36392269+AzeTheGreat@users.noreply.github.com>
2023-07-09 11:43:11 -0700

    Feat: Accelerate trail fading
    The more faded trails are when destroyed, the less noticeable it is.
    Fade speed could be configured such that trails always fade in time to
    not exceed the trail cap, but fading too quickly can also become
    jarring.

---
+++ src/FootstepPerformance/FastFadeTrails.cs
@@ -0,0 +1,31 @@
+﻿using HarmonyLib;
+using System;
+using System.Collections.Generic;
+
+namespace PerformanceFixes
+{
+    [HarmonyPatch(typeof(TrailObject), nameof(TrailObject.Update))]
+    static class FastFadeTrails
+    {
+        public static float DegSpeedOutside => 1f;
+        public static float DegSpeedInside => 25f;
+        public static float DegSpeedOutsideRain => 100f;
+
+        static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> codes)
+        {
+            return codes
+                .ReplaceConstant(TrailObject.DEGRADE_SPEED_OUTSIDE, () => DegSpeedOutside)
+                .ReplaceConstant(TrailObject.DEGRADE_SPEED_INSIDE, () => DegSpeedInside)
+                .ReplaceConstant(TrailObject.DEGRADE_SPEED_OUTSIDE_RAIN, () => DegSpeedOutsideRain);
+        }
+
+        private static IEnumerable<CodeInstruction> ReplaceConstant(this IEnumerable<CodeInstruction> codes, float oldVal, Func<float> func)
+        {
+            var call = Transpilers.EmitDelegate(func);
+            return new CodeMatcher(codes)
+                .SearchForward(i => i.LoadsConstant(oldVal))
+                .Set(call.opcode, call.operand)
+                .InstructionEnumeration();
+        }
+    }
+}
