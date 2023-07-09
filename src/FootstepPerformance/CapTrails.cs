5ba07e9ba66ea17fd1d0ea1d29a7c581be4b6b54
Aze <36392269+AzeTheGreat@users.noreply.github.com>
2023-07-09 11:23:32 -0700

    Feat: Cap the maximum number of footprints
    Destroy the oldest footprint (trail) when the total count exceeds a
    threshold.
    
    Huge numbers of trails can accumulate in one area, dropping performance
    excessively even on high powered desktops.  On weaker devices (e.g.,
    Steam Deck), this impact is even more significant.  I believe the
    performance degradation is due to the lack of batching, but this
    appears to be impossible to alter after Unity is built.
    
    Removing trails that are visible to the camera is jarring.  This
    implementation waits until they are no longer visible before destroying
    to minimize the visual impact.

---
+++ src/FootstepPerformance/CapTrails.cs
@@ -0,0 +1,37 @@
+﻿using HarmonyLib;
+using System.Linq;
+using UnityEngine;
+
+namespace PerformanceFixes
+{
+    [HarmonyPatch(typeof(LeaveTrailComponent), nameof(LeaveTrailComponent.LeaveTrail))]
+    class CapTrails
+    {
+        public static int MaxTrails => 100;
+
+        static void Postfix()
+        {
+            if (MaxTrails == -1)
+                return;
+
+            var allTrails = LeaveTrailComponent._all_trails;
+            if (allTrails.Count > MaxTrails)
+            {
+                var oldestTrail = allTrails.First();
+                LeaveTrailComponent.OnTrailObjectDestroyed(oldestTrail);
+                oldestTrail.gameObject.AddComponent<DestroyTrailWhenInvisible>();
+            }
+        }
+
+        class DestroyTrailWhenInvisible : MonoBehaviour
+        {
+            void Start()
+            {
+                if (!GetComponent<SpriteRenderer>().isVisible)
+                    Destroy(gameObject);
+            }
+
+            void OnBecameInvisible() => Destroy(gameObject);
+        }
+    }
+}
