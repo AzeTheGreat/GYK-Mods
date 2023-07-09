f8eae1f0d5d1850e7f2f433697056c9aaa073572
Aze <36392269+AzeTheGreat@users.noreply.github.com>
2023-07-09 11:37:24 -0700

    Fix: Game does not adjust trail alpha correctly
    The game tries to fade trails out, but sets the alpha incorrectly.
    This causes trails to stay at their initial alpha until they are
    destroyed.

---
+++ src/FootstepPerformance/FixTrailAlpha.cs
@@ -0,0 +1,33 @@
+﻿using HarmonyLib;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace PerformanceFixes
+{
+    [HarmonyPatch(typeof(TrailObject), nameof(TrailObject.Update))]
+    class FixTrailAlpha
+    {
+        static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> codes)
+        {
+            var getColor_getter = AccessTools.PropertyGetter(typeof(SpriteRenderer), nameof(SpriteRenderer.color));
+            var setAlpha_method = AccessTools.Method(typeof(ExtentionTools), nameof(ExtentionTools.SetAlpha));
+
+            return new CodeMatcher(codes)
+                // Remove the getColor call, leaving just the SpriteRenderer on the stack.
+                .SearchForward(i => i.Calls(getColor_getter))
+                .RemoveInstruction()
+                // Replace the setAlpha call.
+                .SearchForward(i => i.Calls(setAlpha_method))
+                .SetInstruction(Transpilers.EmitDelegate(SetAlphaFixed))
+                .InstructionEnumeration();
+
+            // The game's implementation of SetAlpha just gets the color property and sets the alpha, which means it is never applied to the sprite.
+            static void SetAlphaFixed(SpriteRenderer spr, float realAlpha)
+            {
+                var color = spr.color;
+                color.a = realAlpha;
+                spr.color = color;
+            }
+        }
+    }
+}
