4de922ffc79d44b4b68e9926f15f7a5c85a2f0c5
Aze <36392269+AzeTheGreat@users.noreply.github.com>
2023-07-13 21:09:25 -0700

    Fix: Invalid trail types cause missing trails
    When moving between ground types, the game sets the previous ground as
    the trail type so that it can be tracked onto the current ground.  Not
    all ground types are valid trail types (e.g., rocks don't leave a
    trail).  Invalid trail types just early out, which causes long
    stretches of missing trails until the dirty amount decreases enough for
    the main reset mechanism to activate.
    
    Selectively setting the trail decrease to 0 causes this mechanism to
    activate immediately.  These values are correctly set by the game, but
    this behavior was (intentionally or accidentally) disabled by the flag.
    This change appears to have no negative side effects.

---
+++ src/FootstepPerformance/FixMissingTrails.cs
@@ -0,0 +1,24 @@
+﻿using AzeLib.Extensions;
+using HarmonyLib;
+using System.Collections.Generic;
+using System.Reflection.Emit;
+
+namespace FootprintPerformance
+{
+    [HarmonyPatch(typeof(LeaveTrailComponent), MethodType.Constructor, new[] { typeof(BaseCharacterComponent), typeof(string) })]
+    class FixSpritelessTrail
+    {
+        static void Postfix(LeaveTrailComponent __instance)
+        {
+            foreach (var def in __instance._trail_definition.trails)
+            {
+                // If a trail type does not have any sprites, it should immediately "run out" so that the feet can be dirtied with a valid trail type.
+                if (!def.HasAnyTrailSprites())
+                {
+                    def.custom_trail_decrease = true;
+                    def.trail_decrease = 0f;
+                }
+            }
+        }
+    }
+}
