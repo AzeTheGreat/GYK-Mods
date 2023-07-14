4e0b9f914730aa875d9a06c50d176679f64dde9f
Aze <36392269+AzeTheGreat@users.noreply.github.com>
2023-07-13 22:03:40 -0700

    Fix: Trail type changes cause a skipped trail
    When the dirty amount is too low, the game skips the current trail and
    updates the trail type.  If the new ground does not have trails (e.g.,
    grass), this matches expectations: the dirt runs out and the player
    continues not leaving trails on the grass.  However, if the new ground
    does have trails (e.g., dirt), there is a skipped trail: the first step
    onto the dirt does not leave a trail, and everything after does.
    
    This skip is not noticeable when unmodded because the invalid trail
    types cause even longer skips.

--- src/FootstepPerformance/FixMissingTrails.cs
+++ src/FootstepPerformance/FixMissingTrails.cs
@@ -21,4 +21,34 @@ namespace FootprintPerformance
             }
         }
     }
+
+    [HarmonyPatch(typeof(LeaveTrailComponent), (nameof(LeaveTrailComponent.LeaveTrail)))]
+    class FixTrailSwitchSkip
+    {
+        static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> codes)
+        {
+            var getByType_method = AccessTools.Method(typeof(TrailDefinition), nameof(TrailDefinition.GetByType));
+            var trailType_field = AccessTools.Field(typeof(LeaveTrailComponent), nameof(LeaveTrailComponent._trail_type));
+
+            // Find the byType local store.
+            var storeByTypeLoc_code = new CodeMatcher(codes)
+                .SearchForward(i => i.Calls(getByType_method))
+                .SearchForward(i => i.IsStloc())
+                .Instruction;
+
+            return new CodeMatcher(codes)
+                // Update the byType local when the trailType is updated if the dirtyAmount is too low.
+                .SearchForward(i => i.StoresField(trailType_field))
+                .InsertAfter(
+                    new CodeInstruction(OpCodes.Ldarg_0),
+                    Transpilers.EmitDelegate(GetUpdatedTrailDef),
+                    storeByTypeLoc_code.Clone())
+                .SearchForward(i => i.opcode == OpCodes.Ret)
+                // Remove the return from this block since the updated trailType allows leaving a trail.
+                .Set(OpCodes.Nop, null)
+                .InstructionEnumeration();
+
+            TrailTypeDefinition GetUpdatedTrailDef(LeaveTrailComponent cmp) => cmp._trail_definition.GetByType(cmp._trail_type);
+        }
+    }
 }
