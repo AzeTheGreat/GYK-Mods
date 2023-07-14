33667bf0b22e748d0c82578c023daf6552e72513
Aze <36392269+AzeTheGreat@users.noreply.github.com>
2023-07-14 14:50:37 -0700

    Feat: Improve trail updates when switching ground
    Lower the threshold to switch trail types when the new ground type has
    its own trails.
    
    The game will apply the old trail type to the new ground until it
    reaches 10% alpha.  This leads to leaving very faint trails on the new
    ground when the player expects more visible trails.  (e.g., when moving
    from dirt onto swamp.)
    
    Raising this value in all cases causes trails to feel abruptly cut off.

---
+++ src/FootstepPerformance/OverrideTrailUpdateThresh.cs
@@ -0,0 +1,34 @@
+﻿using AzeLib.Extensions;
+using HarmonyLib;
+using System.Collections.Generic;
+using System.Reflection.Emit;
+
+namespace FootprintPerformance
+{
+    [HarmonyPatch(typeof(LeaveTrailComponent), nameof(LeaveTrailComponent.LeaveTrail))]
+    static class OverrideTrailUpdateThresh
+    {
+        static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> codes)
+        {
+            var unmoddedThresh = 0.1f;
+
+            return new CodeMatcher(codes)
+                .SearchForward(i => i.LoadsConstant(unmoddedThresh))
+                .Set(OpCodes.Ldarg_0, null)
+                .InsertAfter(Transpilers.EmitDelegate(GetNewThreshold))
+                .InstructionEnumeration();
+
+            float GetNewThreshold(LeaveTrailComponent cmp)
+            {
+                if (cmp._trail_type != cmp._ground_under)
+                {
+                    // Only override the threshold if the new ground type has its own trails.
+                    var groundDef = cmp._trail_definition.GetByType(cmp._ground_under);
+                    if (groundDef.HasAnyTrailSprites())
+                        return 0.5f;
+                }
+                return unmoddedThresh;
+            }
+        }
+    }
+}
